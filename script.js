2e63e1aaddb0cf8afc4c92a6bfb6bd6dbf45d18a
jacobhcs <jaharr1999@gmail.com>
2023-12-11 02:00:39 -0500

    asdf

--- script.js
+++ script.js
@@ -12,11 +12,12 @@ var temp = document.querySelectorAll('.temp');
 var select = document.querySelector('#change-temp');
 
 function changeMeasure() {
+  var celsiustemps = [24, 18, 27, 19, 21, 16, 26, 21];
   for (var i = 0; i < temp.length; i++) {
     if (select.value == 'F') {
       temp[i].innerText = Math.round((temp[i].innerText * (9 / 5)) + 32);
     } else if (select.value == 'C') {
-      temp[i].innerText;
+      temp[i].innerText = celsiustemps[i];
     } else {
       console.log('aint workin')
     }
